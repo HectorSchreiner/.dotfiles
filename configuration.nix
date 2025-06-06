0a2118868d273a88fdc761a5f32615f255049176
Hector <agenthector007@gmail.com>
2025-06-06 15:17:42 +0200

    hardware stuff

--- configuration.nix
+++ configuration.nix
@@ -42,6 +42,8 @@
      xrdp           #rdp
      btop
 
+     fprintd
+
      p7zip
      zip
      unzip
@@ -193,7 +195,6 @@
   virtualisation.docker = {
     enable = true;
   };
-
   
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -253,16 +254,6 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  #programs.zsh = {
-   # enableCompletion = true;
-    #autosuggestion.enable = true;
-    #syntaxHighlighting.enable = true;
-  #};
 
   users.users.hector = {
     isNormalUser = true;
@@ -328,35 +319,6 @@
 
   services.open-webui.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
- 
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   programs.dconf.enable = true;
  
  programs.nh = {
@@ -378,9 +340,6 @@
     spiceUSBRedirection.enable = true;
   };
   services.spice-vdagentd.enable = true;
-
- 
-
 
   system.stateVersion = "24.05"; # Did you read the comment?
 
